663a606f04a79298de041390f9f3834aac38f125
Omkar Murthy. P <93238595+omkar-252003@users.noreply.github.com>
2022-03-04 20:17:28 +0530

    charstring.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char str1[20],str2[30];
+    printf("Enter name:");
+    scanf("%s",str1);
+    
+    printf("Enter website name:");
+    scanf("%s",str2);
+    
+    printf("Entered name:%s \n",str1);
+    printf("Entered website name:%s \n",str2);
+    return 0;
+}
